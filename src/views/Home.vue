b7b976ca331b6d2d5b48c060dd42c5938a60ecd4
Tom Scharstein <trscharstein@gmail.com>
2020-04-25 03:27:31 -0700

    640px height

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="centered">
       <div class="sample-wrapper">
-        <example :route="hashPath" name="brutalism-api" app="ILST" theme="darkest" :gradient="0" height="600px" />
+        <example :route="hashPath" name="brutalism-api" app="ILST" theme="darkest" :gradient="0" height="640px" />
       </div>
     </div>
 </template>
