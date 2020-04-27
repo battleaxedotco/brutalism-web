3e6fb710fc638c047a4a7a33739ab2bb9aa36a99
Tom Scharstein <trscharstein@gmail.com>
2020-04-27 01:38:04 -0700

    Child route message being received

--- src/App.vue
+++ src/App.vue
@@ -13,15 +13,16 @@
 export default {
   mounted() {
     window.addEventListener('message', (evt => {
+      if (!/localhost|battleaxe.dev/.test(evt.origin)) return null;
       if (!/\{/.test(evt.data)) {
-        console.log(evt.data, this.$route.path);
+        // console.log(evt.data, this.$route.path);
         if (evt.data !== this.$route.path)
           const self = this;
-          console.log('Force it to happen')
+          // console.log('Force it to happen')
           this.$nextTick(() => {
             self.$router.push(evt.data);
             self.$nextTick(() => {
-              console.log('New route is:', this.$route.path)
+              // console.log('New route is:', this.$route.path)
             })
           })
       }
